9b401625005aba3d03ed56e8eccc6e9b75ca73b3
SAKTHIVAISHNAVI N <sakthivaishnavi12@gmail.com>
2025-04-23 00:21:31 +0530

    join update

--- frontend/src/Components/Join/Join.jsx
+++ frontend/src/Components/Join/Join.jsx
@@ -11,8 +11,8 @@ export default function Join () {
     <div className={styles.outerContainer}>
       <div className={styles.innerContainer}>
         <h1 className={styles.heading}>Join</h1>
-        <div><input placeholder='Name' type='text'  className={styles.inputField} onChange={(event)=>setName(event.target.value)} /></div>
-        <div><input placeholder='Room' type='text'  className={styles.inputField} onChange={(event)=>setRoom(event.target.value)} /></div>
+        <div><input placeholder='Name' type='text'  className={styles.inputField} onChange={(event)=>setName(event.target.value)} /></div><br/>
+        <div><input placeholder='Room' type='text'  className={styles.inputField} onChange={(event)=>setRoom(event.target.value)} /></div><br/>
         <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
           <button className={styles.button} type='submit'>Join</button>
         </Link>
